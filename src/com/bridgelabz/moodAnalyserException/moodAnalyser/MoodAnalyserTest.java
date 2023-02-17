4d1c33ae9fe8495258afdb42e205151b13c8ad2f
Sreedhar2484 <nagallisreedhar1918@gmail.com>
2023-02-17 20:39:29 +0530

    added : program to Handle Exception if user when entered invalid mood

--- src/com/bridgelabz/moodAnalyserException/moodAnalyser/MoodAnalyserTest.java
+++ src/com/bridgelabz/moodAnalyserException/moodAnalyser/MoodAnalyserTest.java
@@ -26,10 +26,10 @@ public class MoodAnalyserTest {
     }
 
     @Test
-    public void moodAnalyser() {
-        String ans = object.moodAnalyser();
-        Assert.assertEquals("HAPPY", ans);
+    public void moodAnalyser() throws MoodAnalyserException {
+        String result = object.moodAnalyser();
+        Assert.assertEquals("HAPPY", result);
         ;
-        System.out.println(ans);
+        System.out.println(result);
     }
 }
